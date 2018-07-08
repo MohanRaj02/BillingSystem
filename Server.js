0be031e60dabfe8dc2b7371952f96b5ef03536fc
MohanRaj02 <mohanraj0209@gmail.com>
2018-07-08 13:36:40 +0530

    ServerFile

--- Server.js
+++ Server.js
@@ -3,19 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const mysql = require('mysql');
-
+//var session = require('express-session');
 const app = express();
 
 var corsOptions = {
   origin: 'http://example.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-
 app.use(express.static('public'));
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+//app.use(session({secret:'123'}));
 const con = mysql.createConnection({
   host:"localhost",user:"root",password:"",database:"billingsystem"
 });
@@ -49,16 +48,17 @@ app.post('/add', function (req, res) {
 app.post('/newuser',function(req,res){
   const body = JSON.stringify(req.body);
 
-  const un=req.body.un;
-  const em=req.body.em;
+  const un = req.body.un;
+  const em = req.body.em;
   const pswd = req.body.pswd;
   const ph = req.body.ph;
 
-
   var sql = 'insert into Users(Username,Email,Password,Phone)VALUES(?,?,?,?)'
   con.query(sql,[un,em,pswd,ph],function(err,result){
-             if(err)
+             if(err){
+               console.log(err);
               res.send(`failed`);
+            }
             else {
               res.set('Content-Type', 'text/plain')
               res.send(`success`);
@@ -72,8 +72,8 @@ app.post('/get',function(req,res){
     if(error)
       throw error;
     else {
-      //res.send(rows);
-      console.log(rows);
+      res.send(rows);
+      //console.log(rows);
     }
   });
 })
@@ -83,14 +83,18 @@ app.post('/user',function(req,res){
 
   const email = req.body.email;
   const password = req.body.password;
+
   console.log(email);
   console.log(password);
-  var sql = 'select Email,Password from Users where Email = ? and Password =?';
+  var sql = 'select Userid,Email,Password from Users where Email = ? and Password =?';
   con.query(sql,[email,password],function(err,result,fields){
 
       if(result.length==1){
+        //req.session.id = email;
+        console.log(result[0].Userid);
+        //console.log(req.session.id);
         console.log(result);
-        res.send(`loggedin`);
+        res.set('400').send(String(result[0].Userid));
       }
       else{
         res.send(`Check Email/Password`);
@@ -98,6 +102,35 @@ app.post('/user',function(req,res){
   });
   res.set('Content-Type', 'text/plain');
 })
+
+app.get('/hello', function(req, res){
+  res.send('working');
+});
+
+app.get('/generate', function(req, res){
+  console.log('sad');
+  /*if(req.query.pid && req.query.quantity) {
+
+  }
+  else {
+    return;
+  }*/
+  const pid = req.query.pid;
+  const quantity = req.query.quantity;
+  var sql = "SELECT * FROM product_details WHERE Product_id = ?";
+  con.query(sql, [pid], function(err, result){
+    console.log(quantity + ' = ' + result[0].Quantity);
+    if(result[0].Quantity < quantity) {
+      res.set('Content-Type', 'text/plain');
+      res.send('Product Stock Not Available');
+    }
+    else {
+      res.set('Content-Type', 'text/html');
+      res.sendFile('public/Billing.html', {root: __dirname});
+    }
+  });
+});
+
 app.listen(8000, () => {
   console.log('Server started!');
 });
